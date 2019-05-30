9bd870bab0223f682eae39301f7af29bad0968f9
hejiamintudi <2031862523@qq.com>
2019-05-30 20:55:10 +0800

    tz 的to by 后面可以添加缓冲了

--- assets/NewScript.js
+++ assets/NewScript.js
@@ -15,7 +15,7 @@ cc.Class({
     	cc.kk = tz(hjm._b, "haha", ()=>{
     		cc.log("nihao");
     	}).moveTo(0.5, -100, 0)
-    					  .to([...arr], [1, cc.v2(200, 0)],1, cc.v2(-100, 100))
+    					  .to([...arr], [1, cc.v2(200, 0)],1, cc.v2(-100, 100), cc.easeIn(3.0))
     					  .by([cc.color(100, 100, 100), 3], 0.5, cc.v2(0, -100))(NaN)();
 
     	// cc.kk = tz().to([...arr], [cc.v2(200, 0)], cc.v2(0, -100))
