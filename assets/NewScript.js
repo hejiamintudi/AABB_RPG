21d6312c2f95ddc34d26812e501405a9bb97d227
hejiamintudi <2031862523@qq.com>
2019-05-30 20:39:01 +0800

    tz 添加了to by 函数，可以同时处理多个节点

--- assets/NewScript.js
+++ assets/NewScript.js
@@ -1,13 +1,3 @@
-// Learn cc.Class:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
-
 cc.Class({
     extends: cc.Component,
 
@@ -15,16 +5,24 @@ cc.Class({
     },
 
     start () {
-
+    	cc.jj = this;
     },
 
-    onEnable () {
-        cc.warn("onEnable");
+    kk () {
+    	let node = hjm._a;
+    	let arr = node.getChildren();
+    	// tz(this.node).to([node], 1, cc.v2(0, 0))();
+    	cc.kk = tz(hjm._b, "haha", ()=>{
+    		cc.log("nihao");
+    	}).moveTo(0.5, -100, 0)
+    					  .to([...arr], [1, cc.v2(200, 0)],1, cc.v2(-100, 100))
+    					  .by([cc.color(100, 100, 100), 3], 0.5, cc.v2(0, -100))(NaN)();
+
+    	// cc.kk = tz().to([...arr], [cc.v2(200, 0)], cc.v2(0, -100))
+    	// 		.to([...arr], [0.2, cc.v2(100, 0), [-20]], 1, cc.v2(-200, -100))();
+    	return arr;
     },
 
-    onDisable () {
-        cc.warn("onDisable");
-    }
 
     // update (dt) {},
 });
